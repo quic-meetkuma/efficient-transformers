8cec9ac4687f142d2aaec0422d35b510ce097879
Meet Patel <meetkuma@qti.qualcomm.com>
2025-06-24 18:43:27 +0530

    Updated test id names.
    Signed-off-by: meetkuma <meetkuma@qti.qualcomm.com>

--- tests/finetune/test_finetune.py
+++ tests/finetune/test_finetune.py
@@ -56,7 +56,7 @@ configs = [
         1.00232565,  # expected_train_metric
         0.02060609,  # expected_eval_loss
         1.02081990,  # expected_eval_metric
-        id="llama_config",  # config name
+        id="llama_config_samsum",  # config name
     ),
     pytest.param(
         "meta-llama/Llama-3.2-1B",  # model_name
@@ -74,7 +74,7 @@ configs = [
         1.0043447,  # expected_train_metric
         0.0117334,  # expected_eval_loss
         1.0118025,  # expected_eval_metric
-        id="llama_config",  # config name
+        id="llama_config_gsm8k",  # config name
     ),
     pytest.param(
         "meta-llama/Llama-3.2-1B",  # model_name
@@ -92,7 +92,7 @@ configs = [
         1.0006101,  # expected_train_metric
         0.0065296,  # expected_eval_loss
         1.0065510,  # expected_eval_metric
-        id="llama_config",  # config name
+        id="llama_config_alpaca",  # config name
     ),
     pytest.param(
         "google-bert/bert-base-uncased",  # model_name
@@ -110,7 +110,7 @@ configs = [
         0.55554199,  # expected_train_metric
         0.00738618,  # expected_eval_loss
         0.70825195,  # expected_eval_metric
-        id="bert_config",  # config name
+        id="bert_config_imdb",  # config name
     ),
 ]
 
